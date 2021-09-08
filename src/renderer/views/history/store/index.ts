c197ce071630ba3792a51a87b6ef7c786788903d
SnailDOS <zarasoonich@hotmail.com>
2021-09-08 12:20:04 +0800

    Fix Selected Items Delete & Add.SVG file & Fix White Screen Bug

--- src/renderer/views/history/store/index.ts
+++ src/renderer/views/history/store/index.ts
@@ -208,7 +208,9 @@ export class Store {
 
   @action
   public deleteSelected() {
-    this.removeItems(this.selectedItems);
+    this.selectedItems.forEach(item => {
+      this.removeItems([item])
+    });
     this.selectedItems = [];
   }
 }
