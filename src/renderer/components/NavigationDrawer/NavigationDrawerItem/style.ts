c658650225b0c61fc512681e1270f70f0b1ba936
m-doescode <80221594+m-doescode@users.noreply.github.com>
2022-03-22 17:51:36 +0100

    Updated NavigationDrawerItem style to add transitions + selected color

--- src/renderer/components/NavigationDrawer/NavigationDrawerItem/style.ts
+++ src/renderer/components/NavigationDrawer/NavigationDrawerItem/style.ts
@@ -14,12 +14,16 @@ export const StyledNavigationDrawerItem = styled.div`
   align-items: center;
   position: relative;
   cursor: pointer;
+  transition: 0.2s background-color;
 
   ${({ theme, selected }: { theme?: ITheme; selected?: boolean }) => css`
+    background-color: ${(selected ? 'rgba(0, 0, 0, 0.084)' : 'white')};
+  
     &:hover {
-      background-color: ${theme['pages.lightForeground']
+      background-color: ${!selected ? (theme['pages.lightForeground']
         ? 'rgba(255, 255, 255, 0.06)'
-        : 'rgba(0, 0, 0, 0.04)'};
+        : 'rgba(0, 0, 0, 0.04)') : 'auto'
+      };
     }
 
     &:before {
